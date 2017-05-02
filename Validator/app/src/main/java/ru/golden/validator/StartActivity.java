49590627deb1a3288f72ae847dba3372c4b553de
Matthew Khadzhiev <khad-mat123@yandex.ru>
2017-05-02 23:19:34 +0600

    Получаем данные, начинаем разбор

--- Validator/app/src/main/java/ru/golden/validator/StartActivity.java
+++ Validator/app/src/main/java/ru/golden/validator/StartActivity.java
@@ -12,12 +12,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import ru.golden.validator.network.GetDataService;
 
 public final class StartActivity extends AppCompatActivity implements View.OnClickListener {
     private MyBroadcastReceiver myBroadcastReceiver;
     private static final String EXTRA_STRING_URL = "ru.golden.validator.URL";
     private static final String ACTION_GET_DATA_FROM_URL = "ru.golden.validator.GET_DATA";
     private static final String EXTRA_RESPONSE_STRING = "ru.golden.validator.RESPONSE_STRING";
+
     private Button getDataButton;
     private TextInputEditText inputUrl;
     private TextView exampleTextView;
@@ -33,7 +37,10 @@ public final class StartActivity extends AppCompatActivity implements View.OnCli
 
         inputUrl = (TextInputEditText) findViewById(R.id.input_url);
 
+        exampleTextView = (TextView) findViewById(R.id.example_text_view);
+        exampleTextView.setOnClickListener(this);
 
+        statusTextView = (TextView) findViewById(R.id.status_text_view);
 
         myBroadcastReceiver = new MyBroadcastReceiver();
         final IntentFilter intentFilter = new IntentFilter(ACTION_GET_DATA_FROM_URL);
@@ -45,15 +52,45 @@ public final class StartActivity extends AppCompatActivity implements View.OnCli
     private class MyBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(final Context context, final Intent intent) {
+            final String response = intent.getStringExtra(EXTRA_RESPONSE_STRING);
 
+            if ("".equals(response)) {
+                Toast.makeText(StartActivity.this, R.string.toast_bad_url, Toast.LENGTH_LONG).show();
+            }
+
+            statusTextView.setText(response);
+
+            activateWidgets();
         }
     }
 
     @Override
     public void onClick(final View view) {
-
+        switch (view.getId()) {
+            case R.id.url_button:
+                statusTextView.setText(R.string.text_view_status_download);
+                deactivateWidgets();
+                final Intent intent = new Intent(StartActivity.this, GetDataService.class);
+                intent.putExtra(EXTRA_STRING_URL, inputUrl.getText().toString());
+                startService(intent);
+                break;
+            case R.id.example_text_view:
+                inputUrl.setText(R.string.example_url);
+                break;
+        }
     }
 
+    private void deactivateWidgets() {
+        exampleTextView.setEnabled(false);
+        inputUrl.setEnabled(false);
+        getDataButton.setEnabled(false);
+    }
+
+    private void activateWidgets() {
+        exampleTextView.setEnabled(true);
+        inputUrl.setEnabled(true);
+        getDataButton.setEnabled(true);
+    }
 
     @Override
     protected void onPause() {
@@ -61,7 +98,7 @@ public final class StartActivity extends AppCompatActivity implements View.OnCli
         try {
             unregisterReceiver(myBroadcastReceiver);
         } catch (final Throwable e) {
-            Log.i("MainActivity", "Не отписался Reciever");
+            Log.i("StartActivity", "Не отписался Reciever");
         }
 
     }
